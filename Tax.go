1a1e29db9b4abce10692744f6bc9a1d5a27c99f8
Yasmin Audrey <yasmin_audrey_datario@dlsu.edu.ph>
2023-04-09 19:50:09 +0800

    Tax Calculator

--- Tax.go
+++ Tax.go
@@ -22,8 +22,12 @@ func main() {
 	salaryEntry := widget.NewEntry()
 	salaryEntry.SetPlaceHolder("Please Type in Your Salary Here")
 
+	empTypeRadio := widget.NewRadioGroup([]string{"Employee", "Self-Employed"}, func(selected string) {
+	})
+	empTypeRadio.SetSelected("Employee")
+
 	calculateButton := widget.NewButton("Calculate", func() {
-		handleCalculateButtonClick(w, salaryEntry)
+		handleCalculateButtonClick(w, salaryEntry, empTypeRadio)
 	})
 
 	title := canvas.NewText("Personal Tax Calculator", color.White)
@@ -37,6 +41,7 @@ func main() {
 			container.NewMax(title),
 			widget.NewLabelWithStyle("MONTHLY SALARY**", fyne.TextAlignCenter, fyne.TextStyle{Italic: true}),
 			salaryEntry,
+			empTypeRadio,
 			calculateButton,
 			layout.NewSpacer(),
 			layout.NewSpacer(),
@@ -44,21 +49,28 @@ func main() {
 	)
 
 	w.SetContent(content)
-	w.Resize(fyne.NewSize(350, 450))
+	w.Resize(fyne.NewSize(350, 550))
 	w.SetFixedSize(true)
 	w.ShowAndRun()
 }
 
-func handleCalculateButtonClick(w fyne.Window, salaryEntry *widget.Entry) {
+func handleCalculateButtonClick(w fyne.Window, salaryEntry *widget.Entry, empTypeRadio *widget.RadioGroup) {
+	empType := 0
+	if empTypeRadio.Selected == "Self-Employed" {
+		empType = 1
+	}
+
 	salary, err := strconv.ParseFloat(salaryEntry.Text, 64)
 	if err != nil {
-		fmt.Println("Invalid Input:", err)
+		salaryEntry.SetText(fmt.Sprintf("Invalid Input (Only numbers are allowed)"))
 		return
 	}
 
-	sssCon, phCon, piCon, totalCon := computeTotalContributions(salary)
+	sssCon, phCon, piCon, totalCon := computeTotalContributions(salary, empType)
+
 	taxableIncome := salary - totalCon
 	incomeTax := computeWithholdingTax(taxableIncome)
+
 	netIncome := computeNetSalary(salary, totalCon, incomeTax)
 
 	title := canvas.NewText("Personal Tax Calculator", color.White)
@@ -77,40 +89,63 @@ func handleCalculateButtonClick(w fyne.Window, salaryEntry *widget.Entry) {
 		widget.NewLabel("Net Income"), widget.NewLabel(fmt.Sprintf("%.2f", netIncome)),
 	)
 
-	w.SetContent(container.NewPadded(
+	content := container.NewPadded(
 		container.NewVBox(
 			layout.NewSpacer(),
 			container.NewMax(title),
 			salaryEntry,
 			layout.NewSpacer(),
 			widget.NewButton("Calculate", func() {
-				handleCalculateButtonClick(w, salaryEntry)
+				handleCalculateButtonClick(w, salaryEntry, empTypeRadio)
 			}),
 			layout.NewSpacer(),
+			empTypeRadio,
 			resultTable,
 			layout.NewSpacer(),
 		),
-	))
+	)
+
+	w.SetContent(content)
 }
 
-func computeTotalContributions(salary float64) (float64, float64, float64, float64) {
-	sssContribution := computeSSSContribution(salary)
-	phContribution := computePHContribution(salary)
-	piContribution := computePAGIBIGContribution(salary)
+func computeTotalContributions(salary float64, empType int) (float64, float64, float64, float64) {
+	sssContribution := computeSSSContribution(salary, empType)
+	phContribution := computePHContribution(salary, empType)
+	piContribution := computePAGIBIGContribution(salary, empType)
 	return sssContribution, phContribution, piContribution, (sssContribution + phContribution + piContribution)
 }
 
-func computeSSSContribution(salary float64) float64 {
+func computeSSSContribution(salary float64, empType int) float64 {
 	incomeRange := getSSSIncomeRange()
-	contributionRange := getSSSContributionRange()
+	contributionRange := getSSSContributionRange(empType)
+	mPFRange := getSSSMandatoryProvidentFundRange(empType)
 
 	for i := 0; i < 45; i++ {
 		switch {
 		case i != 44 && (salary >= incomeRange[i] && salary < incomeRange[i+1]):
-			return contributionRange[i]
+			if empType == 1 {
+				if i >= 25 && i < 35 {
+					return contributionRange[i] + 30
+				} else if i >= 35 {
+					return contributionRange[i] + mPFRange[i-35] + 30
+				} else {
+					return contributionRange[i] + 10
+				}
+			} else {
+				if i < 35 {
+					return contributionRange[i]
+				} else if i >= 35 {
+					return contributionRange[i] + mPFRange[i-35]
+				}
+
+			}
 
 		case i == 44 && salary >= incomeRange[i]:
-			return contributionRange[i]
+			if empType == 1 {
+				return contributionRange[i] + mPFRange[i-35] + 30
+			} else {
+				return contributionRange[i] + mPFRange[i-35]
+			}
 		}
 	}
 
@@ -124,33 +159,81 @@ func getSSSIncomeRange() []float64 {
 	for i := 1; i < 44; i++ {
 		incomeRange[i+1] = incomeRange[i] + 500
 	}
-
 	return incomeRange
 }
 
-func getSSSContributionRange() []float64 {
+func getSSSContributionRange(empType int) []float64 {
 	contributionRange := make([]float64, 45)
-	contributionRange[0] = 135.00
-	for i := 0; i < 44; i++ {
-		contributionRange[i+1] = contributionRange[i] + 22.5
+	var interval, fixed float64
+
+	if empType == 1 {
+		contributionRange[0] = 390.00
+		interval = 65
+		fixed = 2600
+	} else {
+		contributionRange[0] = 135.00
+		interval = 22.5
+		fixed = 900
 	}
 
+	for i := 0; i < 44; i++ {
+		if i < 34 {
+			contributionRange[i+1] = contributionRange[i] + interval
+		} else {
+			contributionRange[i+1] = fixed
+		}
+
+	}
 	return contributionRange
 }
 
-func computePHContribution(salary float64) float64 {
-	return salary * 0.04
+func getSSSMandatoryProvidentFundRange(empType int) []float64 {
+	mPFRange := make([]float64, 10)
+	var interval float64
+	if empType == 1 {
+		interval = 65
+	} else {
+		interval = 22.5
+	}
+	mPFRange[0] = interval
+
+	for i := 1; i < 10; i++ {
+		mPFRange[i] += mPFRange[i-1] + interval
+	}
+	return mPFRange
 }
 
-func computePAGIBIGContribution(salary float64) float64 {
+func computePHContribution(salary float64, empType int) float64 {
+	if empType == 0 {
+		return (salary * 0.04) / 2
+	} else {
+		return salary * 0.04
+	}
+
+}
+
+func computePAGIBIGContribution(salary float64, empType int) float64 {
+	contri := 0.0
 	switch {
 	case salary >= 1000 && salary <= 1500:
-		return salary * 0.01
+		contri = salary * 0.01
 	case salary > 1500:
-		return salary * 0.02
+		if salary*0.02 < 100 {
+			contri = salary * 0.02
+		} else {
+			contri = 100
+		}
 	default:
-		return 0
+		contri = 0
 	}
+	if empType == 1 {
+		if salary*0.02 < 100 {
+			contri += salary * 0.02
+		} else {
+			contri += 100
+		}
+	}
+	return contri
 }
 
 func computeWithholdingTax(salary float64) float64 {
